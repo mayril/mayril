e2fc777b07105bef4874e26c5027b6fe93e63a7e
mayril <inho806901@gmail.com>
2022-07-29 17:39:43 +0900

    dd

--- src/bakjoon/Baek2750.java
+++ src/bakjoon/Baek2750.java
@@ -1,9 +1,9 @@
-package codetest;
+package bakjoon;
 
 import java.util.Scanner;
 
 
-public class Main {
+public class Baek2750 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 	
