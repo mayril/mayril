ac827d450d10dbae16e51538863b490cbe17c7eb
mayril <inho806901@gmail.com>
2022-08-05 17:51:17 +0900

    dd

--- src/codetest/Solution.java
+++ src/codetest/Solution.java
@@ -14,9 +14,12 @@ public static void main(String args[]) throws Exception
 
 	for(int test_case = 1; test_case <= T; test_case++)
 	{	
-		
-		System.out.println("#"+test_case+" ");
-		
+		//vscode에서 display none과 visibility none의 차이
+		//id 다음에오는 span태그 //<&lt ? >&gt
+ 		System.out.println("#"+test_case+" ");
+		//nth child
+ 		
+ 		//flex box
 		
 	/////////////////////////////////////////////////////////////////////////////////////////////
 	
