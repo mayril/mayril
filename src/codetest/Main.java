ffc2e6fc7eaa8fd3d57fb58ca4e06c7013ddd760
mayril <inho806901@gmail.com>
2022-08-08 08:35:29 +0900

    Update Main.java

--- src/codetest/Main.java
+++ src/codetest/Main.java
@@ -6,26 +6,39 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a=sc.nextInt();
-		int []arr=new int[a];
-		for (int i = 0; i < a; i++) {
-			arr[i]=sc.nextInt();
-		}
-		int cnt=0;
-	
-		int max=0; //약수의 최대값
-		int min=arr[0];//약수의 최소값
-		for (int i = 0; i < a; i++) {
-			if (arr[i]>max) {
-				max=arr[i];
-			}
-			if (arr[i]<min) {
-				min=arr[i];
-			}
-			
-		}
 		
-		System.out.println(min*max);
+		int stu=sc.nextInt();
+		
+		int[][] classone=new int[stu][5];
+		int[] cnt=new int[stu];
+		for (int i = 0; i < stu; i++) {
+			for (int j = 0; j < 5; j++) {
+				classone[i][j]=sc.nextInt();
+			}
+		}
+		int t=0;
+		
+			for (int i = 0; i < stu; i++) {
+					for (int j = 0; j < stu; j++) {
+						for (int h = 0; h < 5; h++) {
+						if (classone[i][h]==classone[j][h]) {
+							cnt[i]++;
+							break;
+						}
+					}
+			}
+		}
+
+
+		int max=0;
+		int maxt=0;
+		for (int i = 0; i < cnt.length; i++) {
+			if(cnt[i]>maxt) {
+				maxt=cnt[i];
+				max=i;
+			}
+		}
+		System.out.println(max+1);
 		//-----------------------
 	}
 	
