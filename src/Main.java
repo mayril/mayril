eb46b09cfdb5e6e08280dc7a8eedc6c713ec874d
mayril <inho806901@gmail.com>
2022-08-17 18:00:38 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.EmptyStackException;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 
 public class Main {
@@ -15,19 +16,73 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int w=Integer.parseInt(st.nextToken());
+		int h=Integer.parseInt(st.nextToken());
+		st=new StringTokenizer(br.readLine());
+		int x=Integer.parseInt(st.nextToken());
+		int y=Integer.parseInt(st.nextToken());
+		int time=Integer.parseInt(br.readLine());
+		int fx=x;
+		int fy=y;
+		int direction=1;
 		
-		//int t=Integer.parseInt(br.readLine());
-		String ad=br.readLine();
-		String [] add=ad.split("");
-		for (int i = 0; i < add.length; i++) {
-			if (add[i].equals("+")) {
-				
-			}else {
-				System.out.print(add[i]);
+		for (int i = 0; i < time; i++) {
+			if (direction==1) {
+				fx+=1;
+				fy+=1;
+			}else if (direction==2) {
+				fx-=1;
+				fy-=1;
+			}else if (direction==3) {
+				fx-=1;
+				fy+=1;
+			}else if (direction==4) {
+				fx+=1;
+				fy-=1;
 			}
+			if (fx==w&&fy==h||fx==w&&fy==0||fx==0&&fy==0||fx==0&&fy==h) {
+				if (direction==1) {
+					direction=2;
+				}else if (direction==2) {
+					direction=1;
+				}else if (direction==3) {
+					direction=4;
+				}else if (direction==4) {
+					direction=3;
+				}
+			}else {
+				if (fx==w) {
+					if (direction==1) {
+						direction=3;
+					}else {
+						direction=2;
+					}
+				}else if (fx==0) {
+					if (direction==2) {
+						direction=3;
+					}else {
+						direction=1;
+					}
+				}else if (fy==h) {
+					if (direction==1) {
+						direction=4;
+					}else {
+						direction=2;
+					}
+				}else if (fy==0) {
+					if (direction==4) {
+						direction=1;
+					}else {
+						direction=3;
+					}
+				}
+			}
+			System.out.println(fx+" "+fy);
 		}
 		
+		System.out.println(fx+" "+fy);
 		//-----------------------
 	}
 
