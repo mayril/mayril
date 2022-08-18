a002445e3cdeb6275135d9994626f2fadcedcfa2
mayril <inho806901@gmail.com>
2022-08-18 17:57:44 +0900

    dd

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,56 @@
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 
 public class Main {
+
 	public static void main(String[] args) throws NumberFormatException, IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		//Scanner sc = new Scanner(System.in);
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int w=Integer.parseInt(st.nextToken());
-		int h=Integer.parseInt(st.nextToken());
-		st=new StringTokenizer(br.readLine());
-		int x=Integer.parseInt(st.nextToken());
-		int y=Integer.parseInt(st.nextToken());
-		int time=Integer.parseInt(br.readLine());
-		int fx=x;
-		int fy=y;
-		int direction=1;
-		
-		for (int i = 0; i < time; i++) {
-			if (direction==1) {
-				fx+=1;
-				fy+=1;
-			}else if (direction==2) {
-				fx-=1;
-				fy-=1;
-			}else if (direction==3) {
-				fx-=1;
-				fy+=1;
-			}else if (direction==4) {
-				fx+=1;
-				fy-=1;
-			}
-			if (fx==w&&fy==h||fx==w&&fy==0||fx==0&&fy==0||fx==0&&fy==h) {
-				if (direction==1) {
-					direction=2;
-				}else if (direction==2) {
-					direction=1;
-				}else if (direction==3) {
-					direction=4;
-				}else if (direction==4) {
-					direction=3;
-				}
-			}else {
-				if (fx==w) {
-					if (direction==1) {
-						direction=3;
-					}else {
-						direction=2;
-					}
-				}else if (fx==0) {
-					if (direction==2) {
-						direction=3;
-					}else {
-						direction=1;
-					}
-				}else if (fy==h) {
-					if (direction==1) {
-						direction=4;
-					}else {
-						direction=2;
-					}
-				}else if (fy==0) {
-					if (direction==4) {
-						direction=1;
-					}else {
-						direction=3;
-					}
-				}
-			}
-			System.out.println(fx+" "+fy);
+		Scanner sc = new Scanner(System.in);
+		StringTokenizer stringTokenizer=new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(stringTokenizer.nextToken());
+		int k=Integer.parseInt(stringTokenizer.nextToken());
+		int [] arr=new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i]=i+1;
 		}
+	
+		System.out.print("<");
+		int index=0;
+		int index2=0;
 		
-		System.out.println(fx+" "+fy);
+		do {
+			index2=index2%n;
+			int tmp=arr[index2];
+			
+			if (arr[index2]!=0) {
+				index++;
+				if (index%k==0&&index!=n*k) {
+					System.out.print(tmp+","+" ");
+					arr[index2]=0;
+				}else if (index%k==0&&index==n*k) {
+					System.out.print(tmp);
+					break;
+				}
+			}
+			index2++;			
+		} while (index<=n*k);
+		
+		System.out.println(">");
+		//  2 4 2 4 2 0 0
 		//-----------------------
 	}
+
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,82 +1,44 @@
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 
-public class Solution
-{
-		public static int[] arr2;
-		public static int max = 0;
-public static void main(String args[]) throws NumberFormatException, IOException 
-{
+public class Solution{
+
+public static void main(String args[]) throws IOException {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 	Scanner sc = new Scanner(System.in);
-
-	int T=sc.nextInt();
-	for(int test_case = 1; test_case <= T; test_case++)
-	{	
-		max=0;
-		String k=sc.next();	//숫자 입력값
-		int time=sc.nextInt(); //교환횟수
-		String[] arr=k.split("");	
-		arr2=new int[arr.length];
-		// 자릿수 나눠서 배열에 집어넣음
-		for (int i = 0; i < arr2.length; i++) {
-			arr2[i]=Integer.parseInt(arr[i]);
-		}
-
-		dfs(0, 0, time);
-		
-		System.out.println("#" + test_case + " " + max);
-	/////////////////////////////////////////////////////////////////////////////////////////////	
-	}
-	}
-	public static void change(int a,int b) {
-		int tmp=0;
-		tmp=arr2[a];
-		arr2[a]=arr2[b];
-		arr2[b]=tmp;
-	}
-	
-	public static void dfs(int index,int cnt,int time) {		
-		if (time==cnt) {
-			StringBuffer sb = new StringBuffer();
-            for(int i : arr2)
-                sb.append(i);
-            // 최대값 비교
-            max = Math.max(max, Integer.parseInt(sb.toString()));
-            return;
-		}
-		//전 범위 탐색 (시작 인덱스 ~ 끝)
-		for (int i = index; i < arr2.length-1; i++) {
-			 // 전 범위 탐색(시작 + 1 ~ 끝)
-			for (int j = i+1; j < arr2.length; j++) {
-				if(arr2[i] <= arr2[j]) {
-					change(i, j);
-					//for (int j2 = 0; j2 < arr2.length; j2++) {
-					//	System.out.print(arr2[j2]);
-					//}
-					//System.out.println();
-					dfs(i, cnt+1, time);	
-					//System.out.println(max);
-					
-					change(i, j);
-				}
+	int T=Integer.parseInt(br.readLine());
+	//int T=10;
+	for(int test_case = 1; test_case <= T; test_case++){
+		Stack<String>stack=new Stack<>();
+		String k=br.readLine();
+		String[] arr=k.split("");
+		int sum=0;
+		int t=1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("(")) {
+				stack.push("(");
+				sum++;
+			}else {
+				stack.pop();
+				sum--;
+				sum+=stack.size();
 			}
+			System.out.println(sum);
 		}
+		//(((1(11))(1)1))(11)
+		System.out.println("#"+test_case+" "+sum);
 	}
 
-	
+	}
 }
 
