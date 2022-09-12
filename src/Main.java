1b08f6ab2b5da6c57f4841d3bedc5a9b38768049
mayril <inho806901@gmail.com>
2022-09-12 22:38:14 +0900

    dd

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,29 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.StringTokenizer;
+
 
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException  {
+
+	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		Scanner sc = new Scanner(System.in);
+		//StringBuilder sb=new StringBuilder();
+		int n= sc.nextInt();
+		int result=1;
+		for (int i = 1; i <= n; i++) {
+			result*=i;
+		}
 		
-		StringTokenizer st1=new StringTokenizer(br.readLine());
-		
-	
-		System.out.println();
+		System.out.println(result);
 		//-----------------------
 	}
-	
-	public static class deque {
-		private int front;
-		private int rear;
-		private int size;
-		private int[] de;
-		
-		//생성자 1 초기
-		public deque() {
-			super();
-			this.front = 0;
-			this.rear = 0;
-			this.size = 0;
-			this.de = new int[size];
-		}
-		
-		//생성자 2 용적할당
-		public deque(int size) {
-			super();
-			this.front = 0;
-			this.rear = 0;
-			this.size = 0;
-			this.de = new int[size];
-		}
-		
-	}
-	
-	
-	
-	
+
 }
